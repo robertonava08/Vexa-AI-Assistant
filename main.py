ad5be5710404d830a3fef88287921d9fc562b41a
robertonava08 <robtop378@gmail.com>
2025-04-20 20:22:40 -0500

    First commit with main.py and README.md

---
+++ main.py
@@ -0,0 +1,9 @@
+# Import the necessary libraries
+import openai
+import pyaudio
+import pygame
+import faster_whisper
+import google.cloud.texttospeech as tts
+import numpy as np
+import os
+from dotenv import load_dotenv
